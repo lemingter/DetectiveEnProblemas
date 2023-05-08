b457676ada55c0bd4fdb08aaa5c61d38367ba9d3
lemingter <101127844+lemingter@users.noreply.github.com>
2023-05-08 13:56:53 -0600

    Initial commit

---
+++ Source/DetectiveEnProblemas/DetectiveEnProblemas.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DetectiveEnProblemas.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DetectiveEnProblemas, "DetectiveEnProblemas" );
+ 
