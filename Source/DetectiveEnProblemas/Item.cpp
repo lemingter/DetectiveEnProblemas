fb2f2baf05bd0932bd79e9a3a9816d30be2d26f2
AlvaroBishop <bishopgamma@gmail.com>
2023-05-11 09:05:45 -0600

    finish items

--- Source/DetectiveEnProblemas/Item.cpp
+++ Source/DetectiveEnProblemas/Item.cpp
@@ -1,5 +1,6 @@
 #include "Item.h"
 
+#include "DetectiveEnProblemasGameMode.h"
 #include "MeshAttributes.h"
 #include "Components/BoxComponent.h"
 #include "Engine/Engine.h"
@@ -12,7 +13,7 @@ AItem::AItem()
 	PrimaryActorTick.bCanEverTick = true;
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
-	CollisionBox->SetBoxExtent(FVector(32.0f,32.0f,32.0f));
+	CollisionBox->SetBoxExtent(FVector(35.0f,35.0f,35.0f));
 	CollisionBox->SetCollisionProfileName("Trigger");
 	RootComponent = CollisionBox;
 	SuperMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
@@ -41,7 +42,11 @@ void AItem::Tick(float DeltaTime)
 
 void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	GEngine->AddOnScreenDebugMessage(-1,1.f,FColor::Yellow, "OverlapBeginFunctionCalled");
+	ADetectiveEnProblemasGameMode* MyGameMode = Cast<ADetectiveEnProblemasGameMode>(GetWorld()->GetAuthGameMode());
+	if (MyGameMode != nullptr)
+	{
+		MyGameMode->Counter ++;
+	}
 
 	this->Destroy();
 }
--- Source/DetectiveEnProblemas/Item.h
+++ Source/DetectiveEnProblemas/Item.h
@@ -14,6 +14,7 @@ class DETECTIVEENPROBLEMAS_API AItem : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AItem();
+	
 
 protected:
 	// Called when the game starts or when spawned
